631dedaf57149816df5429c188d04061e606c65b
Victoria  Topham <topham.victoria@gmail.com>
2020-03-14 15:13:21 -0700

    got a reset in and am working on a header

--- src/App.js
+++ src/App.js
@@ -2,33 +2,36 @@ import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import "./App.css";
 
+import Header from "./Components/Header/header.js";
 
 
 
 function AddPhoto() {
-  const [APOD, setAPOD] = useState("");
+  const [APOD, setAPOD] = useState({});
 
   useEffect(() => {
-    Axios.get("https://api.nasa.gov/planetary/apod?api_key=JzkrP8ReT0T7KHnBYhfFAkXgm624q5ndkE6Gg5uv")
+    Axios.get("https://api.nasa.gov/planetary/apod?api_key=JzkrP8ReT0T7KHnBYhfFAkXgm624q5ndkE6Gg5uv&date=2020-01-01")
     .then((response) => {
       setAPOD(response.data);
     });
-  });
+  },[]);
 
   return (
-    <img src = {APOD.hdurl} alt = "today's picture from space" />
+    <img src = {APOD.url} alt = {APOD.explanation} />
   );
 }
 
 
-function AddPhoto() {
+function App() {
   return (
+    
     <div className="App">
+      <Header/>
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun::: 🚀!
       </p>
-      <Photo />
+      <AddPhoto />
     </div>
   );
 }
---
+++ src/Components/Header/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./header.less";
+
+function Header() {
+
+    return (
+      <header>
+        <h1 className = "title">NASA APOD</h1>
+        <p>A Gallery Of NASA's A Photo A Day Pictures</p>
+      </header>
+  
+    );
+  }
+
+export default Header;
