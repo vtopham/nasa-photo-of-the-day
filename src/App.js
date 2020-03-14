4d44a6c040dac4f3a4c10c858ba5fb1643613763
Victoria  Topham <topham.victoria@gmail.com>
2020-03-14 14:44:56 -0700

    got a photo added and running ls

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 import "./App.css";
 
-function App() {
+
+
+
+function AddPhoto() {
+  const [APOD, setAPOD] = useState("");
+
+  useEffect(() => {
+    Axios.get("https://api.nasa.gov/planetary/apod?api_key=JzkrP8ReT0T7KHnBYhfFAkXgm624q5ndkE6Gg5uv")
+    .then((response) => {
+      setAPOD(response.data);
+    });
+  });
+
+  return (
+    <img src = {APOD.hdurl} alt = "today's picture from space" />
+  );
+}
+
+
+function AddPhoto() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <Photo />
     </div>
   );
 }
