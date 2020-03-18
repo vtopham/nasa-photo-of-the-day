61b2bdfeb45dbc97a4362b688bf97caf1a4b23e3
Victoria  Topham <topham.victoria@gmail.com>
2020-03-18 10:35:37 -0700

    pulled the date scroller out into its own component in the photoscroller folder

---
+++ src/Components/PhotoScroller/datepicker.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+function DatePicker(props) {//this will insert buttons to pick a date to load a new photo!
+    const {chosenDate, setChosenDate, year, month, day} = props;
+    
+    function setNewDate() {
+        const newYear = document.getElementById("year").value;
+        const newMonth = document.getElementById("month").value;
+        const newDay = document.getElementById("day").value;
+    
+        setChosenDate(`${newYear}-${newMonth}-${newDay}`);
+        console.log(chosenDate);
+    }
+    
+    return (//returns the date chooser with today set as the default
+        <div className = "date-picker">
+            <p>Pick a date!</p>
+            <div className = "date-holder"> 
+                <label htmlFor="year">YYYY:</label> <input type="text" id="year" name="year" defaultValue = {year}></input>
+                <label htmlFor="year">MM:</label> <input type="text" id="month" name="month" defaultValue = {month}></input>
+                <label htmlFor="year">DD:</label> <input type="text" id="day" name="day" defaultValue = {day}></input>
+                <button onClick = {setNewDate} >Submit</button>
+            </div>
+        </div>
+    )
+    }
+
+export default DatePicker;
--- src/Components/PhotoScroller/photoscroller.js
+++ src/Components/PhotoScroller/photoscroller.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import DatePicker from "./datepicker"
 import "./photoscroller.css";
 
 
@@ -10,6 +11,7 @@ function AddPhoto(props) { //This will insert the photo that is chosen into the 
       Axios.get(`https://api.nasa.gov/planetary/apod?api_key=JzkrP8ReT0T7KHnBYhfFAkXgm624q5ndkE6Gg5uv&date=${chosenDate}`)
       .then((response) => {
         setAPOD(response.data);
+        console.log(response);
       });
     },[chosenDate]);
   
@@ -21,27 +23,7 @@ function AddPhoto(props) { //This will insert the photo that is chosen into the 
   }
 
 
-function DatePicker(props) {//this will insert buttons to pick a date to load a new photo!
-const {chosenDate, setChosenDate, year, month, day} = props;
 
-function setNewDate() {
-    const newYear = document.getElementById("year").value;
-    const newMonth = document.getElementById("month").value;
-    const newDay = document.getElementById("day").value;
-
-    setChosenDate(`${newYear}-${newMonth}-${newDay}`);
-    console.log(chosenDate);
-}
-
-return (//returns the date chooser with today set as the default
-    <div className = "date-holder"> 
-        <label htmlFor="year">YYYY:</label> <input type="text" id="year" name="year" defaultValue = {year}></input>
-        <label htmlFor="year">MM:</label> <input type="text" id="month" name="month" defaultValue = {month}></input>
-        <label htmlFor="year">DD:</label> <input type="text" id="day" name="day" defaultValue = {day}></input>
-        <button onClick = {setNewDate} >Submit</button>
-    </div>
-)
-}
 
 
 
@@ -57,6 +39,7 @@ const [chosenDate, setChosenDate] = useState(`${year}-${month}-${day}`)
 return (
     <div className = "photo-scroller">
         <AddPhoto APOD = {APOD} setAPOD = {setAPOD} chosenDate = {chosenDate} setChosenDate = {setChosenDate}/>
+        
         <DatePicker chosenDate = {chosenDate} setChosenDate = {setChosenDate} year = {year} month = {month} day = {day}/>
     </div>
     );
